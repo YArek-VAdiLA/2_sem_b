34e7b7139b90c70da48ed38c84c4ec2d883c7ed4
YArek-VAdiLA <91405345+YArek-VAdiLA@users.noreply.github.com>
2025-02-25 20:23:26 +0300

    Header.h

--- 1.5/Header.h
+++ 1.5/Header.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include<iostream>
+#include <cmath> 
+
+using namespace std;
+
+
+
+double function(double x, double t);
+double integrate(double a, double b, double t, double eps, int& k_iter);
